5cc1b2357f3043e662cb9f535525f04947791a00
Alex Kuzmin <alex.kuzmin@proton.me>
2025-04-16 16:49:56 +0800

    Measure RAM usage of SHA256 benchmark code

--- sp1/algos/sha256/script/src/bin/main.rs
+++ sp1/algos/sha256/script/src/bin/main.rs
@@ -9,9 +9,17 @@
 //! ```shell
 //! RUST_LOG=info cargo run --release -- --prove
 //! ```
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 use clap::Parser;
+use memory_stats::memory_stats;
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
+
+use jemalloc_ctl::{
+    epoch,
+    stats::{self},
+};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 pub const SHA_ELF: &[u8] = include_elf!("sha-program");
@@ -56,8 +64,27 @@ fn main() {
 
         println!("Number of cycles: {}", report.total_instruction_count());
     } else {
+        epoch::advance().unwrap();
+        let allocated_before = stats::allocated::read().unwrap();
+        let resident_before = stats::resident::read().unwrap();
+
         // Setup the program for proving.
+        let usage_before = memory_stats().unwrap();
         let (pk, vk) = client.setup(SHA_ELF);
+        let usage_after = memory_stats().unwrap();
+        println!(
+            "memory_stats: Setup memory usage: {} MB resident | {} MB virt",
+            (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0),
+            (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0)
+        );
+        epoch::advance().unwrap();
+        let allocated_after = stats::allocated::read().unwrap();
+        let resident_after = stats::resident::read().unwrap();
+        println!(
+            "jemalloc: Setup memory usage: {} MB resident | {} MB allocated",
+            (resident_after - resident_before) as f32 / 1024.0 / 1024.0,
+            (allocated_after - allocated_before) as f32 / 1024.0 / 1024.0,
+        );
 
         println!("ELF size: {} KB", pk.elf.len() as f32 / 1024.0);
         let pk_bytes = bincode::serialize(&pk).unwrap();
@@ -66,11 +93,30 @@ fn main() {
             pk_bytes.len() as f32 / (1024.0 * 1024.0)
         );
 
+        epoch::advance().unwrap();
+        let allocated_before = stats::allocated::read().unwrap();
+        let resident_before = stats::resident::read().unwrap();
+
+        let usage_before = memory_stats().unwrap();
         // Generate the proof
         let proof = client
             .prove(&pk, &stdin)
             .run()
             .expect("failed to generate proof");
+        let usage_after = memory_stats().unwrap();
+        println!(
+            "memory_stats: Prover memory usage: {} MB resident | {} MB virt",
+            (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0),
+            (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0)
+        );
+        epoch::advance().unwrap();
+        let allocated_after = stats::allocated::read().unwrap();
+        let resident_after = stats::resident::read().unwrap();
+        println!(
+            "jemalloc: Prover memory usage: {} MB resident | {} MB allocated",
+            (resident_after - resident_before) as f32 / 1024.0 / 1024.0,
+            (allocated_after - allocated_before) as f32 / 1024.0 / 1024.0,
+        );
 
         let proof_bytes = bincode::serialize(&proof).unwrap();
         println!(
