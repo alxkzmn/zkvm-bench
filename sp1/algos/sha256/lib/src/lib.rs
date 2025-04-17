47083bc50b976c0df43eb8b8664fe070c6ca4dd7
Alex Kuzmin <alex.kuzmin@proton.me>
2025-04-17 23:18:32 +0800

    Refactor SHA256 RAM measurement to use vmmap

--- sp1/algos/sha256/lib/src/lib.rs
+++ sp1/algos/sha256/lib/src/lib.rs
@@ -3,7 +3,6 @@ use sha2::{Digest, Sha256};
 pub fn sha2(input: &[u8]) -> u32 {
     let mut hasher = Sha256::new();
     hasher.update(input);
-    let result = hasher.finalize();
-    let res = Into::<[u8; 32]>::into(result);
+    let _ = hasher.finalize();
     1
 }
